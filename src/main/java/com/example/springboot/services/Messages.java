bff3c526c859fd3d766466df96ac1a7b166974de
MICHAELLAPTOP\maima <maimao@gmail.com>
2023-10-25 10:15:01 -0400

    no message

---
+++ src/main/java/com/example/springboot/services/Messages.java
@@ -0,0 +1,18 @@
+package com.example.springboot.services;
+
+public class Messages {
+
+    public String getMessage(String name)
+    {
+        StringBuilder s = new StringBuilder();
+        if(name == null || name.trim().length() == 0)
+        {
+            s = s.append("Please Provide a name!");
+        }
+        else
+        {
+            s.append("Hello " + name + "!");
+        }
+        return s.toString();
+    }
+}
---
+++ src/test/java/com/example/springboot/MessagesTest.java
@@ -0,0 +1,30 @@
+package com.example.springboot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.example.springboot.services.Messages;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessagesTest {
+
+    @Test
+    public void testMessage() {
+        Messages obj = new Messages();
+        Assertions.assertEquals("Hello World!", obj.getMessage("World"));
+    }
+
+    @Test
+    public void testNameBlank() {
+        Messages obj = new Messages();
+        Assertions.assertEquals("Please Provide a name!", obj.getMessage(""));
+    }
+
+    @Test
+    public void testNameNull() {
+        Messages obj = new Messages();
+        Assertions.assertEquals("Please Provide a name!", obj.getMessage(null));
+    }
+
+}
