65b2eafda24994761714d94e149afba07b8a1c7f
MICHAELLAPTOP\maima <maimao@gmail.com>
2023-10-25 15:37:04 -0400

    no message

--- src/test/java/com/example/springboot/services/MessagesTest.java
+++ src/test/java/com/example/springboot/services/MessagesTest.java
@@ -10,19 +10,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class MessagesTest {
 
     @Test
-    public void testMessage() {
+    void testMessage() {
         Messages obj = new Messages();
         Assertions.assertEquals("Hello World!", obj.getMessage("World"));
     }
 
     @Test
-    public void testNameBlank() {
+    void testNameBlank() {
         Messages obj = new Messages();
         Assertions.assertEquals("Please Provide a name!", obj.getMessage(""));
     }
 
     @Test
-    public void testNameNull() {
+    void testNameNull() {
         Messages obj = new Messages();
         Assertions.assertEquals("Please Provide a name!", obj.getMessage(null));
     }
