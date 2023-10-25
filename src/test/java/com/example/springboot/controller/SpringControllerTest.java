c6c11dbe036823e4e55d3879c71f5cf3d14526ce
MICHAELLAPTOP\maima <maimao@gmail.com>
2023-10-25 15:33:29 -0400

    no message

---
+++ src/test/java/com/example/springboot/controller/SpringControllerTest.java
@@ -0,0 +1,30 @@
+package com.example.springboot.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SpringControllerTest {
+	@Autowired
+    private MockMvc mockMvc;
+	
+	@Test
+    void shouldGetDefaultSpringMessage() throws Exception {
+
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Mr. DevOps1")));
+    }
+
+}
